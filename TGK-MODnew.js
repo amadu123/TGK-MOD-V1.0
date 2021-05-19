418da996768f5cb48a3367ecfe1ae64a0720e476
amadu123 <58538816+amadu123@users.noreply.github.com>
2021-05-19 12:49:04 -0400

    Update TGK-MODnew.js

--- TGK-MODnew.js
+++ TGK-MODnew.js
@@ -1208,28 +1208,37 @@ document.addEventListener('keydown', (e)=>{
     millPlacer.start(e.keyCode);
     turretPlacer.start(e.keyCode);
 
-    if(e.keyCode == 82 && document.activeElement.id.toLowerCase() !== 'chatbox') {
-                    say('That\'s fast!')
-        acc(18);
-        hat(7);
+     if(e.keyCode == 82 && document.activeElement.id.toLowerCase() !== 'chatbox') {
+        autoaim=true;
+        doNewSend(["ch", ["TGK MOD HOP U LOVE IT"]]);
         doNewSend(["5", [primary, true]]);
-        autoaim = true;
-        autoprimary = true;
-        autosecondary = false;
-                doNewSend(["7", [1]]);
-        setTimeout( () => {
-                        autoprimary = false;
-            autosecondary = true;
-            doNewSend(["5", [secondary, true]]);
-            hat(53);
-            setTimeout( () => {
-                    doNewSend(["7", [1]]);
-                normal();
-                autoaim = false;
-                autosecondary = false;
-                doNewSend(["5", [primary, true]]);
+        doNewSend(["13c", [0, 7, 0]]);
+        doNewSend(["13c", [0, 0, 1]]);
+        doNewSend(["13c", [0, 18, 1]]);
+        doNewSend(["2", [90**15]]);
+        doNewSend(["c", [1]]);
+
+          setTimeout( () => {
+                doNewSend(["13c", [0, 53, 0]]);
+              doNewSend(["2", [90**15]]);
+                doNewSend(["5", [secondary, true]]);
             }, 125);
-            }, delay);
+
+        setTimeout( () => {
+            doNewSend(["5", [primary, true]]);
+            doNewSend(["c", [0, null]]);
+            doNewSend(["13c", [0, 21, 1]]);
+            doNewSend(["13c", [0, 6, 0]]);
+            autoaim=false;
+        }, 250);
+        setTimeout( () => {
+            doNewSend(["ch", ["reloading"]]);
+            doNewSend(["5", [secondary, true]]);
+        }, 1000);
+        setTimeout( () => {
+            doNewSend(["ch", ["reloaded..."]]);
+            doNewSend(["5", [primary, true]]);
+        }, 1550);
     }
 
     if(e.keyCode == 90 && document.activeElement.id.toLowerCase() !== 'chatbox') {
@@ -2030,4 +2039,3 @@ for(i = 0;i < count;i++)
 {
     areplacer[0].parentNode.removeChild(areplacer[0]);
 }
-
